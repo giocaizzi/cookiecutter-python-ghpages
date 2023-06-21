c300e17f0cdf1c9067f5c9ba36bd33b9726d6ef6
giocaizzi <giocaizzi@gmail.com>
2023-06-22 01:23:56 +0200

    init

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="{{cookiecutter.package_name}}",
+    version="0.0.1  ",
+    description="{{cookiecutter.short_description}}",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url="{{cookiecutter.url}}",
+    author="{{cookiecutter.author_name}}",
+    author_email=f"{{cookiecutter.author_email}}",
+    license="{{cookiecutter.license}}",
+    packages=find_packages(include=["{{cookiecutter.package_name}}", "{{cookiecutter.package_name}}/*"]),
+    setup_requires=[],
+    tests_require=[],
+    install_requires=[
+    ],
+    extras_require={
+        "docs": [],
+        "dev": [],
+    },
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    project_urls={
+        "Documentation": "https://{{cookiecutter.github_username}}.github.io/{{cookiecutter.package_name}}/",
+        "Bug Reports": "https://github.com/{{cookiecutter.github_username}}/{{cookiecutter.package_name}}/issues",
+        "Source": "https://github.com/{{cookiecutter.github_username}}/{{cookiecutter.package_name}}",
+    },
+)
---
+++ {{cookiecutter.package_name}}/__init__.py
@@ -0,0 +1,8 @@
+"""{{cookiecutter.package_name}}"""
+
+__author__ = "{{cookiecutter.author_name}}"
+__email__ = "{{cookiecutter.author_email}}"
+__license__ = "{{cookiecutter.license}}"
+__url__ = "{{cookiecutter.url}}"
+
+__version__ = "0.0.1"
